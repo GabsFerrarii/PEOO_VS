93ba6d993bafee9f12433b1438a7ee48cc7af3a4
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2022-10-17 15:52:56 -0300

    Upload de Agenda da Aula02

---
+++ Aula02/AgendaApp/Contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaApp
+{
+    class Contato
+    {
+        private string nome, gomes, telefone;
+        public Contato(string n, string g, string t)
+        {
+            nome = n;
+            gomes = g;
+            telefone = t;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {gomes} - {telefone}";
+        }
+    }
+}
