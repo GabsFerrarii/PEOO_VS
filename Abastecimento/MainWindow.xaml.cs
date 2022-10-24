21f6a8e0a0ada668f27ffac04b9d8c4348d60fbd
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2022-10-24 15:58:01 -0300

    Inseri os exercicios feitos na aula 4

---
+++ Abastecimento/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AbastecimentoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento abastecimento = new Abastecimento();
+            abastecimento.SetKmRodados(double.Parse(txt1.Text));
+            abastecimento.SetValorPago(double.Parse(txt2.Text));
+            abastecimento.SetValorLitro(double.Parse(txt3.Text));
+            txt4.Text = abastecimento.MediaKmLitro().ToString();
+            txt5.Text = abastecimento.MediaReaisKm().ToString();
+        }
+    }
+}
---
+++ RetanguloApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RetanguloApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Retangulo retangulo = new Retangulo();
+            retangulo.SetBase(double.Parse(txt1.Text));
+            retangulo.SetAltura(double.Parse(txt5.Text));
+            txt3.Text = retangulo.CalcArea().ToString();
+            txt4.Text = retangulo.CalcDiagonal().ToString();
+        }
+    }
+}
