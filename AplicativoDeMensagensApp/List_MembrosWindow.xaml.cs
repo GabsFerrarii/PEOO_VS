c8302f9de2a7f5c6eb6d7b9d5646684d897a490d
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2023-01-27 10:43:47 -0300

    Protótipo das janelas do AplicativoDeMensagensApp

---
+++ AplicativoDeMensagensApp/List_MembrosWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AplicativoDeMensagensApp
+{
+    /// <summary>
+    /// Lógica interna para List_MembrosWindow.xaml
+    /// </summary>
+    public partial class List_MembrosWindow : Window
+    {
+        public List_MembrosWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
